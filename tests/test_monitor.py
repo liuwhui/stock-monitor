6172fc2ff6a8ce49692c4be26d90662411a7ff5a
liuwhui <liuwhui@notes>
2026-06-25 14:02:10 +0800

    chore: lower dividend ETF alert threshold

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -81,7 +81,7 @@ class MonitorTest(unittest.TestCase):
         self.assertEqual([item.symbol for item in items], ["518850", "515180", "600941"])
         dividend = next(item for item in items if item.symbol == "515180")
         self.assertEqual(dividend.name, "中证红利ETF")
-        self.assertEqual(dividend.alert_below, 1.315)
+        self.assertEqual(dividend.alert_below, 1.3)
         mobile = next(item for item in items if item.symbol == "600941")
         self.assertEqual(mobile.alert_below, 86)
 
